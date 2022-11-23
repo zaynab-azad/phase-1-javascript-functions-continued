4c2ad36ea83b347040dec87a2286ccf0681c454d
Zaynab Azad Khdir <zaynabbbazad20@gmail.com>
2022-11-23 21:39:11 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // code your solution here
+function saturdayFun(arg = "roller-skate") {
+  return `This Saturday, I want to ${arg}!`;
+}
+
+function mondayWork(arg = "go to the office") {
+  return `This Monday, I will ${arg}.`;
+}
+function wrapAdjective(arg) {
+  return function (argg = "special") {
+    return `You are ${arg}${argg}${arg}!`;
+  };
+}
